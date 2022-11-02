107a4b48fa969725b828d9abe368819c07177d45
melakadil <adil-salih@hotmail.com>
2022-11-02 17:30:40 +0200

    week2

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,2 +1,25 @@
 'use strict';
 // catRoute
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.send('From this endpoint you can get cats')
+})
+router.get('/:id',  (req, res) => {
+
+  res.send(`You requested a cat whose id is ${req.params.id}`);
+});
+
+// POST method route
+router.post('/', (req, res) => {
+  res.send('From this endpoint you can post cats')
+})
+router.put('/', (req, res) => {
+  res.send('From this endpoint you can put cats')
+})
+router.delete('/', (req, res) => {
+  res.send('From this endpoint you can delete cats')
+})
+module.exports = router;
