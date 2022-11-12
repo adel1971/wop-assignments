02664830aac556869d9c38ea045a7d31796cca3b
melakadil <adil-salih@hotmail.com>
2022-11-12 23:19:37 +0200

    validation start

--- week2/routes/userRoute.js
+++ week2/routes/userRoute.js
@@ -1,17 +1,19 @@
 'use strict';
 // userRoute
-const express = require('express')
-const {user_list_get,user_get, user_post} = require("../controllers/userController");
-const router = express.Router()
+const express = require('express');
+const {user_list_get, user_get, user_post, user_put, user_delete} = require(
+    '../controllers/userController');
+const {body} = require('express-validator');
+const router = express.Router();
 
-router.get('/', user_list_get);
-router.get('/:id', user_get);
-// POST method route
-router.post('/', user_post);
-router.put('/', (req, res) => {
-  res.send('From this endpoint you can put users');
-});
-router.delete('/', (req, res) => {
-  res.send('From this endpoint you can delete users')
-})
+router.route('/').
+get(user_list_get).
+post(body('name').isLength({min: 3}).escape(),
+    body('email').isEmail(),
+    body('passwd').matches('(?=.*[\\p{Lu}]).{8,}'),
+    user_post).
+put(user_put);
+
+router.route('/:id').get(user_get).delete(user_delete);
+
 module.exports = router;
